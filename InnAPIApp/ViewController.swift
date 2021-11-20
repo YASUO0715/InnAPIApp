e273096fe0f97d4b0c12272debef7fe0ad12f738
YASUO0715 <yasuo0715niihori@gmail.com>
2021-11-20 16:34:03 +0900

    Initial Commit

---
+++ InnAPIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InnAPIApp
+//
+//  Created by Yasuo Niihori on 2021/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
